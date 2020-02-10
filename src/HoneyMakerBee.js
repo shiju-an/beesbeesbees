0f2d993d52803ab23e58ba4766d6d2c9dc2e6bad
Joanna Wang <ohjeezz@Joannas-MacBook-Pro.local>
2020-02-10 11:11:33 -0800

    complete honeymakerbee prototype

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot ++;
+}
+
+HoneyMakerBee.prototype.giveHoney = funtion() {
+  this.honeyPot --;
+}
