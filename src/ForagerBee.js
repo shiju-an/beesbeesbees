87ff7132ca360108fd749e17e9b3312b811a262e
Joanna Wang <ohjeezz@Joannas-MacBook-Pro.local>
2020-02-10 11:15:23 -0800

    complete foragerbee prototype

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
 var ForagerBee = function() {
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+
+ForagerBee.prototype.forage = function() {
+  this.treasureChest.push('treasure');
+}
+ForagerBee.prototype.constructor = ForagerBee;
 
