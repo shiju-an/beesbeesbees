0265652a7860ac78204edc1250502dc826b77aac
Joanna Wang <ohjeezz@Joannas-MacBook-Pro.local>
2020-02-10 11:06:28 -0800

    complete bee prototype

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,8 @@
 var Bee = function() {
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
